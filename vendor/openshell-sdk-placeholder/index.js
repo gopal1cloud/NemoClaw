9e7275d7ad01e7fc59a845380e7da2fc27d6e439
Aaron Erickson <aerickson@nvidia.com>
2026-05-28 15:21:57 -0700

    chore: add temporary OpenShell SDK placeholder

---
+++ vendor/openshell-sdk-placeholder/index.js
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+"use strict";
+
+const MESSAGE =
+  "@openshell/sdk is not published yet. This NemoClaw draft vendors only the SDK type surface " +
+  "so CI can build; replace vendor/openshell-sdk-placeholder with the OpenShell-published " +
+  "@openshell/sdk package before running SDK-backed sandbox operations.";
+
+class OpenShellClient {
+  static async connect() {
+    throw new Error(MESSAGE);
+  }
+}
+
+class OidcRefresher {
+  constructor(initialToken, _initialExpiresAt, callback) {
+    this.token = initialToken;
+    this.callback = callback;
+  }
+
+  currentToken() {
+    return this.token;
+  }
+
+  async refresh() {
+    const refreshed = await this.callback();
+    this.token = refreshed.accessToken;
+    return this.token;
+  }
+}
+
+module.exports = {
+  OidcRefresher,
+  OpenShellClient,
+};
